e21b7f826109dd28c4372750bc361acc51c433ba
yolenlew <yolenlew@163.com>
2023-03-28 15:34:00 +0800

    init:初始化项目

---
+++ src/com/lew/alog/HiAlog.java
@@ -0,0 +1,11 @@
+package com.lew.alog;
+
+/**
+ * @author Yolen
+ * @date 2023/3/28
+ */
+public class HiAlog {
+    public static void main(String[] args) {
+        System.out.println("Hello, algorithm");
+    }
+}
